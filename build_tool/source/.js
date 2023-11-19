972bc8678d9df3b65c8d54e323ee08090683c4a4
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-11-19 10:56:24 +0700

    Update .js

--- build_tool/source/.js
+++ build_tool/source/.js
@@ -849,7 +849,7 @@ class BuildTool {
         }
         catch(err) {
 
-            console.log(Object.keys(err))
+            console.log(err)
 
             console.error(`${temp_module.src_dir} :: ${temp_module.non_virtual_src_file() || temp_module.id} #${err.lineNumber} -> (): failure error`);
 
